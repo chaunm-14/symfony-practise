71d2349bba5edec6dad4aa42afa653609f444366
chaunm-14 <chau_nguyen@fastboy.net>
2025-03-07 09:46:36 +0700

    update source

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\HelloService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HelloController extends AbstractController
 {
+    public function __construct(private readonly HelloService $helloService)
+    {
+    }
+
     #[Route('/hello', name: 'hello')]
     #[IsGranted('ROLE_USER')]
     public function sayHello(): Response
     {
-        return new Response('Hello World!');
+        return new Response($this->helloService->sayHello());
     }
 }
